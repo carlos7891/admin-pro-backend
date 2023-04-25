abbd58d43975544419edeba92f5ae5cd81d53244
carlos.espinosa@segurosbolivar.com-desarrollador <carlos.espinosa@segurosbolivar.com>
2023-04-25 11:39:04 -0500

    Se termina seccion 13

--- controllers/auth.js
+++ controllers/auth.js
@@ -87,7 +87,19 @@ const googleSignIn = async(req, res = response) =>{
     }
 }
 
+const renewToken = async( req, res = response) => {
+    const uid = req.uid;
+    //Generar token
+    const token = await generateJWT(uid)
+
+    res.json({
+        ok:true,
+        token
+    })
+}
+
 module.exports = {
     login,
-    googleSignIn
+    googleSignIn,
+    renewToken
 }
--- routes/auth.js
+++ routes/auth.js
@@ -4,8 +4,9 @@
 
 const { Router } = require('express');
 const { check } = require('express-validator');
-const { login, googleSignIn } = require('../controllers/auth');
+const { login, googleSignIn, renewToken } = require('../controllers/auth');
 const { fieldValidation } = require('../middlewares/fieldValidation');
+const { jwtValidation } = require('../middlewares/jwtValidation');
 
 const router = Router();
 
@@ -26,4 +27,10 @@ router.post('/google',
     googleSignIn
 )
 
+router.get('/renew', 
+    [
+        jwtValidation
+    ],
+    renewToken
+)
 module.exports = router;
